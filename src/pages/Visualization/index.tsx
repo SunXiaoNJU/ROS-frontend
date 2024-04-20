3853f5ad92d3066532b0a4e2c36792b880890329
sunxiao <201250011@samil.nju.edu.cn>
2024-04-20 23:30:09 +0800

    feat: add launch name

--- src/pages/Visualization/index.tsx
+++ src/pages/Visualization/index.tsx
@@ -1,16 +1,66 @@
 import { PageContainer } from '@ant-design/pro-components';
-import { Link } from '@umijs/max';
-import { Alert, Space } from 'antd';
+import { Link, request } from '@umijs/max';
+import { Alert, Button, Input, Space, message, notification } from 'antd';
+import { useState } from 'react';
+import styles from './index.less';
+
+type NotificationType = 'success' | 'info' | 'warning' | 'error';
 
 const Vision: React.FC = () => {
   const loginType = sessionStorage.getItem('accessId') !== '';
+  const [launchCnt, setLaunchCnt] = useState('');
+
+  const [api, contextHolder] = notification.useNotification();
+  const openNotificationWithIcon = (type: NotificationType) => {
+    api[type]({
+      message: '上传launch名称',
+      description: '用户需要上传自己文件中launch的名称',
+    });
+  };
+
+  const submitLaunchRequest = async () => {
+    const res = request(
+      'https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188', // 后端接口
+      {
+        params: {
+          launch: launchCnt,
+        },
+      },
+    );
+    if (await res) {
+      message.success('上传成功！');
+    }
+  };
 
   return loginType ? (
     <PageContainer ghost>
+      {contextHolder}
+      <Space className={styles.msg}>
+        <Button
+          type="primary"
+          onClick={() => openNotificationWithIcon('info')}
+          className={styles.tipButton}
+        >
+          使用提醒
+        </Button>
+      </Space>
+      <Space.Compact style={{ width: '16%' }} className={styles.input}>
+        <Input
+          placeholder="上传文件中launch的名称"
+          value={launchCnt}
+          onChange={(e) => {
+            setLaunchCnt(e.target.value);
+          }}
+        />
+        <Button type="primary" onClick={submitLaunchRequest}>
+          上传
+        </Button>
+      </Space.Compact>
+
       <iframe
-        src="https://webviz.io/app/?rosbridge-websocket-url=ws://121.41.128.141:9090"
+        src="http://116.62.210.218:8080/?rosbridge-websocket-url=ws://121.41.128.141:9090"
         width="100%"
-        height="800em"
+        height="750em"
       ></iframe>
     </PageContainer>
   ) : (
